d734ab51d4d3e8d0f918936b7d16c4ab5bc2b920
utzmankazeem <usman4kay@gmail.com>
2022-04-15 14:12:47 +0100

    commit all files

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# exchange
+# exchangeApp
+
+#An Exchange Api Application 
+
+#An app biult on nodejs engine and Ejs. 
+
+#Consumes the services of bitcoin Average api.
+
+#Created to make available echange rates of bitcoin for conversion into different currency at available market rate using Api of bitcoin average server. 
+
+#Users enter the currency to exchange and chose bitcoin as the coin to convert to.
+
+#our app provides service from bitcoin average api and provides us with the current market rate.
+
+#API #Bitcoin-Average #Nodejs #Ejs #Javascript #Bootstrap #CSS
---
+++ index.js
@@ -0,0 +1,45 @@
+//Jshint esversion: 6
+const express = require("express")
+const request = require("request")
+const app = express()
+app.use(express.urlencoded({extended: true}))
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/", function(req, res){
+    let crypto = req.body.crypto;
+    let fiat = req.body.fiat;
+    let amount = req.body.amount;
+
+    //MAKING REQUEST USING PARAMETERS
+    let options = {
+        url: "https://apiv2.bitcoinaverage.com/convert/global",
+        method: "GET",
+        qs: {
+            from: crypto,
+            to: fiat,
+            amount: amount
+        }
+    };
+    
+    request(options, function(error, response, body){
+        let data = JSON.parse(body);//parsing the json object as js
+        let price = data.price; //or copy the path from the json tree
+        
+        console.log(price);
+
+        let currentDate = data.time;
+
+    })
+    res.write("<p>The current datetime is "+ currentDate +"  </p>")
+    res.write("<h1>" + amount + crypto + " is currently worth "+ price + fiat +" </h1>")
+    res.send()
+    
+    })
+
+
+app.listen(3000, function() {
+    console.log("server is running on port 3000")
+})
